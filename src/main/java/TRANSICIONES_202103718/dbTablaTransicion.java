7da6dfc219682668ecd734703a0125ecb5475edd
H4rr1ss <98117359+H4rr1ss@users.noreply.github.com>
2023-03-12 10:00:44 -0600

    [add]: Inicio de lógica tabla transiciones

--- src/main/java/TRANSICIONES_202103718/dbTablaTransicion.java
+++ src/main/java/TRANSICIONES_202103718/dbTablaTransicion.java
@@ -5,12 +5,249 @@ import java.util.List;
 import java.util.Map;
 
 public class dbTablaTransicion {
-    private HashMap<String, Object> tabla = new HashMap<>();
-    private ArrayList<HashMap> listaHashMap = new ArrayList<HashMap>();
+    private HashMap<String, elementHash> tabla = new HashMap<>();
+    private Map<String, List<String>> transicionP = new HashMap<>();
+    private List<transicion> listadoF = new ArrayList<transicion>();
+    private int indice = 0;
     
     public void addDatosHash(String nodo, String simbolo, List<String> siguientes){
         elementHash obj = new elementHash(simbolo, siguientes);
         tabla.put(nodo, obj);
-        listaHashMap.add(tabla);
     }
+    
+    
+    // SOLO SERVIRA PARA REALIZAR LAS PRUEBAS DE ESTA CLASE
+    public void PROVISIONAL(String nodo, elementHash objeto){
+        tabla.put(nodo, objeto);
+    }
+    
+    public void returnDatos(){
+        for(Map.Entry<String, elementHash> mapa : tabla.entrySet()){
+            indice = 2;
+            System.out.println(mapa.getKey()+" = "+ mapa.getValue().getSimbolo()+", "+mapa.getValue().getSiguientes());
+        }
+    }
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    private String verificarExistSigPos(List<String> listaSig){// SE COMPARA EL HASH DE TRANSICIONES CON LA LISTA SIGUIENTE Y RETORMA EL ESTADOFINAL
+        if(transicionP.isEmpty()){
+            return "";
+        }
+        
+        for(Map.Entry<String, List<String>> tablaF : transicionP.entrySet()){
+            int contador = 0;
+
+            for(Object cadena :tablaF.getValue().toArray()) {// ARRAY BASE
+                
+                for(String sig : listaSig){ // LISTA SIGUIENTESS
+                    if(cadena.equals(sig)){
+                        contador++;
+                    }
+                }
+                
+            }
+            
+            if (contador == tablaF.getValue().toArray().length){
+                return tablaF.getKey();
+            }
+            
+        }
+        return "";
+    }
+    
+    
+    private boolean aceptacion(List<String> sigpos){
+        for(Map.Entry<String, elementHash> mapa : tabla.entrySet()){
+            if(mapa.getValue().getSimbolo().equals("#")){
+                for(int i = 0; i<sigpos.size(); i++){
+                    if(sigpos.get(i).equals(mapa.getKey())){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    private List<String> rep(List<String> listaF, List<String> listaC){
+        
+        
+        if(listaF.isEmpty()){
+            for(int i = 0; i<listaC.size(); i++){
+                listaF.add(listaC.get(i));
+            }
+        }else{
+            for(int i = 0; i<listaC.size(); i++){
+                int data = 0;
+                
+                for(int j = 0; j<listaF.size(); j++){
+                    if(listaC.get(i).equals(listaF.get(j))){
+                        data++;
+                    }
+                }
+                if(data == 0){
+                    listaF.add(listaC.get(i));
+                }
+            }
+        }
+        return listaF;
+    }
+    
+    /*
+                        1ra. corrida
+                         listaC     listaF
+                         [2,3,4]    []
+                        
+                        2ra. corrida
+                         listaC     listaF
+                         [5,2,6]    [2,3,4]   
+    */
+    
+    private void eliminados(String eliminar, Map<String, elementHash> hashEvaluar){
+        String[] eliminados = eliminar.split(",");
+        
+        for(int i=0; i < eliminados.length; i++){
+            hashEvaluar.remove(eliminados[i]);
+        }
+    }
+    
+    
+    // LOGICA FINAL PARA TABLA DE TRANSICIONES
+    private void SeleccionNodos(String[] S0){//S0 = 1,2,3,4,5,6
+        Map<String, elementHash> hashEvaluar = new HashMap<>();
+        
+        for(Map.Entry<String, elementHash> mapa : tabla.entrySet()){
+            for(int i = 0; i < S0.length; i++){
+                if(S0[i].equals(mapa.getKey())){
+                    // SE CREA UN NUEVO HASHMAP QUE REPRESENTA LA TABLA COLOR AZUL
+                    hashEvaluar.put(S0[i], mapa.getValue());
+                }
+            }
+        }
+        
+        int indiceI = 0;
+        boolean llave = true;
+        
+        while(llave){
+            // TERMINARA EL CICLO HASTA QUE LA TABLA AZUL YA HAYA SIDO EVALUADA TOTALMENTE
+            if(hashEvaluar.isEmpty()){
+                llave = false;
+            }
+            /*
+                SE EMPIEZA A RECORRER LA TABLA CON TODOS LOS ELEMENTOS Y AL MISMO TIEMPO 
+                SE RECORRE LA TABLA AZUL PARA EMPEZAR A CREAR LAS TRANSICIONES, TOMANDO
+                EN CUENTA SOLO LAS QUE EXISTAN EN LA TABLA 
+            */
+            for(Map.Entry<String, elementHash> mapaBase : tabla.entrySet()){
+                
+                String eliminadosS = "";
+                List<String> ListaSig = new ArrayList<String>();
+                List<String> listaF = new ArrayList<String>();
+                
+                for(Map.Entry<String, elementHash> mapAux : hashEvaluar.entrySet()){
+
+                    if(mapaBase.getValue().getSimbolo().equals(mapAux.getValue().getSimbolo())){
+                        //nodo = obj(simbolo, [siguientes])
+                        //me devolverá los arrays de siguientes
+                        
+                        //CREACION DE LOS ATRIBUTOS PARA EL OBJETO TRANSICION
+                        ListaSig = rep(listaF, mapAux.getValue().getSiguientes());// ALMACENA LOS NUEVOS SIGPOS
+                        
+                        eliminadosS += mapAux.getKey()+",";
+                    }
+                    
+                }
+                eliminados(eliminadosS, hashEvaluar);
+                
+                if (hashEvaluar.isEmpty()){
+                    break;
+                }
+                
+                String EstadoF = "";
+                String estado = "";
+                
+                //VERIFICAR SI LA LISTA DE SIGUIENTES YA EXISTE ENTONCES NO SE CREA NUEVO ESTADO
+                if (this.indice !=0){
+                    estado = verificarExistSigPos(ListaSig);// ACA IRA estadoF = [tablaSig]
+                }
+                
+                if (estado.equals("")){
+                    //SI ES TRUE ENTONCES
+                    EstadoF = "S"+String.valueOf(this.indice+1); //REPRESENTA EL ESTADO IGUALADO AL FINAL
+                    this.indice++;
+                }else{
+                   EstadoF = estado; //REPRESENTA EL ESTADO IGUALADO AL FINAL
+                }
+                       
+                String EstadoI = "S"+String.valueOf(indiceI); //ASIGNA UN ESTADO INICIAL INICIA EN 0
+                String simbolo = mapaBase.getValue().getSimbolo();//OBTIENE EL SIMBOLO QUE SE EVALUO
+                        
+                boolean acept = aceptacion(ListaSig);//VERIFICA SI EL NODO # SE ENCUENTRA EN LOS NODOS SIGPOS
+                       
+                transicionP.put(EstadoF, ListaSig);
+                transicion objTransiciones = new transicion(EstadoI, simbolo, ListaSig, EstadoF, acept);
+                this.listadoF.add(objTransiciones);
+            }
+            indiceI++;
+        }
+    }
+    
+    
+    public void TablaTransiciones(String estadoI){
+        String[] S0 = estadoI.split(",");// {1,2,3,4,5,6}
+        
+        // PASO 2
+        SeleccionNodos(S0);
+        
+    }
+
 }
+
--- src/main/java/edu/usac/olc1/proyecto1/Base.java
+++ src/main/java/edu/usac/olc1/proyecto1/Base.java
@@ -1,10 +1,11 @@
 package edu.usac.olc1.proyecto1;
 
 import Interfaz.Menu;
-import java.util.ArrayList;
+
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import TRANSICIONES_202103718.*;
 
 public class Base {
 
@@ -15,10 +16,11 @@ public class Base {
             map.put(key, value);
         }
     }
+    
  
     public static void main(String[] args)
     {
-        Map<String, List<String>> colors = new HashMap<>();
+        /*Map<String, List<String>> colors = new HashMap<>();
  
         colors.put("harry", new ArrayList<>(Arrays.asList("trabajador")));
         colors.put("White", new ArrayList<>(Arrays.asList("#FFFFFF", "#ffffff")));
@@ -36,9 +38,6 @@ public class Base {
         String part1 = parts[0]; // 123
         String part2 = parts[1]; // 654321
                 
-        /*for (int i = 0; i < parts.length; i++) {
-            System.out.println(parts[i]);
-        }*/
         System.out.println("mapa impreso literalmente: "+parts);
         for (int i = 0; i<parts.length; i++){
             System.out.println(parts[i]);
@@ -50,31 +49,70 @@ public class Base {
             System.out.println(mapa.getValue());
             
             
-        }
+        }*/
+
+        // PRUEBAS PARA LA REALIZACION DE LA TABLA DE TRANSICIONES´
         
-        /*
-        if (mapa.getKey().equals("harry")){
-                for (int i = 0; i < mapa.getValue().toArray().length; i++){
-                    //System.out.println(mapa.getValue().toArray()[i]);
-                    if(mapa.getValue().toArray()[i].equals("trabajador")){
-                        ve = true;
-                        break;
-                    }
-                }
+        // STRING OBJ->(STRING, LISTA)        
+        List<String> p1 = new ArrayList<String>();
+        p1.add("5");
+        p1.add("6");
+        List<String> p2 = new ArrayList<String>();
+        p2.add("5");
+        p2.add("6");
+        List<String> p3 = new ArrayList<String>();
+        p3.add("5");
+        p3.add("6");
+        List<String> p4 = new ArrayList<String>();
+        p4.add("5");
+        p4.add("6");
+        List<String> p5 = new ArrayList<String>();
+        p5.add("7");
+        p5.add("8");
+        List<String> p6 = new ArrayList<String>();
+        p6.add("7");
+        p6.add("8");
+        List<String> p7 = new ArrayList<String>();
+        p7.add("7");
+        p7.add("8");
+        List<String> p8 = new ArrayList<String>();
+        p8.add("-");
                 
-                break;
-            }
-        */
+        elementHash e1 = new elementHash("d", p1);
+        elementHash e2 = new elementHash("b", p2);
+        elementHash e3 = new elementHash("c", p3);
+        elementHash e4 = new elementHash("a", p4);
+        elementHash e5 = new elementHash("d", p5);
+        elementHash e6 = new elementHash("b", p6);
+        elementHash e7 = new elementHash("d", p7);
+        elementHash e8 = new elementHash("#", p8);
         
-        System.out.println(ve);
-        System.out.println("RESULTADO  " + ((Object)parts).getClass().getSimpleName());
+        dbTablaTransicion objT = new dbTablaTransicion();
+        objT.PROVISIONAL("1", e1);
+        objT.PROVISIONAL("2", e2);
+        objT.PROVISIONAL("3", e3);
+        objT.PROVISIONAL("4", e4);
+        objT.PROVISIONAL("5", e5);
+        objT.PROVISIONAL("6", e6);
+        objT.PROVISIONAL("7", e7);
+        objT.PROVISIONAL("8", e8);
+       
+
+       
+        //-----------------------------------------------------------------------
         
+        String PrimeroRaiz = "1,2,3,4,5,6,";
+        
+        objT.TablaTransiciones(PrimeroRaiz);
+
+       
+
+/*
+System.out.println("RESULTADO  " + ((Object)parts).getClass().getSimpleName());
+*/
+
+
+
     }
 }
-
-
-
-
-
-
 
